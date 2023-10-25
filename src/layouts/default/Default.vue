025f27a4466bac5ac66466c920d066ce1d387ed2
Daniel Kelly <me@danielkelly.io>
2023-10-25 10:09:55 -0500

    2-create-the-project-and-define-the-page-layout

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -1,12 +1,20 @@
 <template>
-  <v-app>
-    <default-bar />
+  <v-layout class="rounded rounded-md">
+    <v-app-bar
+      title="Application bar"
+    ></v-app-bar>
 
-    <default-view />
-  </v-app>
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item title="Navigation drawer"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main
+      class="d-flex align-center justify-center"
+      style="min-height: 300px"
+    >
+      <router-view></router-view>
+    </v-main>
+  </v-layout>
 </template>
-
-<script lang="ts" setup>
-  import DefaultBar from './AppBar.vue'
-  import DefaultView from './View.vue'
-</script>
