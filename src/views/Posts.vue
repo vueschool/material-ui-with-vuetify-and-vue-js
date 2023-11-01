da92522ecccc7c40716d6fce4a7d86eb2f29d1b0
Daniel Kelly <me@danielkelly.io>
2023-11-01 10:27:43 -0500

    6-vuetify-buttons-and-dialogs

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -45,6 +45,32 @@ const search = ref("");
       item-value="title"
       v-model="selected"
       :search="search"
-    ></v-data-table>
+    >
+      <template #item.title="{ item }">
+        <v-dialog fullscreen>
+          <template v-slot:activator="{ props }">
+            <button v-bind="props">{{ item.title }}</button>
+          </template>
+
+          <template v-slot:default="{ isActive }">
+            <v-card title="Dialog">
+              <v-card-text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+
+                <v-btn
+                  text="Close Dialog"
+                  @click="isActive.value = false"
+                ></v-btn>
+              </v-card-actions>
+            </v-card>
+          </template>
+        </v-dialog>
+      </template>
+    </v-data-table>
   </div>
 </template>
